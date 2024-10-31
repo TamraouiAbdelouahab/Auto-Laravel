5277c426fe030f5127ddd7589b9d03e3d1f29841
TamraouiAbdelouahab <abdelwahabtamraoui@gmail.com>
2024-10-31 07:06:18 +0100

    added Prototype project

---
+++ Prototype/database/migrations/2024_10_30_192647_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('content');
+            $table->date('publication_date');
+            $table->unsignedBigInteger('author_id');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('author_id')->references('id')->on('categories');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
