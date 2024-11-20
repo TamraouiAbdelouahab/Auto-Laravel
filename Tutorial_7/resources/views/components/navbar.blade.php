b590e43f1147125862bbc9bde0e0e71f0a73e938
TamraouiAbdelouahab <abdelwahabtamraoui@gmail.com>
2024-11-20 22:09:17 +0100

    ajouter Tutorial 7

---
+++ Tutorial_7/resources/views/components/navbar.blade.php
@@ -0,0 +1,8 @@
+<nav>
+    <a href="{{Route('articles.index')}}" style="margin-right:10px">
+        Articles
+    </a>
+    <a href="{{Route('articles.create')}}" style="margin-right:10px">
+        Ajouter un article
+    </a>
+</nav>
